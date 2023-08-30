3545ddac496f36021fdc496880febaba996b9bc8
ShoshanaBezalel <91996753+ShoshanaBezalel@users.noreply.github.com>
2023-08-30 18:28:32 +0300

    30.08.2023

---
+++ DAL/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    internal interface IRepository<T>
+    {
+        public bool Create(T entity);
+        public T GetById(int id);
+        public bool Update(T entity);
+        public bool Delete(int id);
+
+    }
+}
---
+++ DAL/PersonRepository.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    internal class PersonRepository : IPersonRepository
+    {
+        LibraryContext context;
+        public PersonRepository(LibraryContext context)
+        {
+            this.context = context;    
+        }
+        public bool Create(Person entity)
+        {
+            try
+            {
+                Person CreateNewPerson = new Person(FirstName, LastName, Password, Phone, Email);
+                var createdPerosn = context.People.Add(CreateNewPerson);
+                context.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Person GetById(int id)
+        {
+            return context.People.FirstOrDefault(p => p.Id == id);
+        }
+
+        public bool Update(Person entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
